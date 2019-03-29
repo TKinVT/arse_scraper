cd69c92caaeb8856709c38fac0f133f0a8f04697
TK <tk@tkinvt.com>
2019-03-29 14:01:14 -0400

    Added an article retrieval function

--- scraper.py
+++ scraper.py
@@ -2,25 +2,29 @@ import requests
 from bs4 import BeautifulSoup
 import datetime
 
-def get_container():
+def make_soup():
     url = 'https://www.bbc.com/sport/football/teams/arsenal'
     r = requests.get(url)
     html = r.text
     soup = BeautifulSoup(html, 'html.parser')
 
+    return soup
+
+def get_scores_fixtures_container():
+    soup = make_soup()
     container = soup.find('div', class_="live-scores-index-container")
 
     return container
 
 def get_scores():
-    scores_container = get_container().find('h2', string="Results").find_next_sibling()
+    scores_container = get_scores_fixtures_container().find('h2', string="Results").find_next_sibling()
     scores_html = scores_container.find_all('ul')
     scores = [format_score(score) for score in scores_html]
 
     return scores
 
 def get_fixtures():
-    fixtures_container = get_container().find('h2', string="Fixtures").find_next_sibling()
+    fixtures_container = get_scores_fixtures_container().find('h2', string="Fixtures").find_next_sibling()
     fixtures_html = fixtures_container.find_all('ul')
     fixtures = [format_fixture(fixture) for fixture in fixtures_html]
 
@@ -83,6 +87,21 @@ def date_obj(unf_date):
 
     return date_object
 
+def get_articles():
+    soup = make_soup()
+    container = soup.find('h2', id="top-stories-title").find_next_sibling()
+    articles_html = container.find_all('h3')
+    articles = [format_article(x) for x in articles_html]
+
+    return articles
+
+def format_article(article_html):
+    article = {
+              "url":article_html.a['href'],
+              "title":article_html.a.text
+              }
+
+    return article
+
 if __name__ == '__main__':
-    print(get_scores())
-    print(get_fixtures())
+    format_table_row()
