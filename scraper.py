2becad96e256c44ccdeb1ea98ad3ede6945155be
tk <tk@tkinvt.com>
2019-04-07 20:38:26 -0400

    Included 'Today' section of score box. Streamlined html format functions into one.

--- scraper.py
+++ scraper.py
@@ -14,50 +14,48 @@ def get_container():
 def get_scores():
     scores_container = get_container().find('h2', string="Results").find_next_sibling()
     scores_html = scores_container.find_all('ul')
-    scores = [format_score(score) for score in scores_html]
+    scores = [format_html(score) for score in scores_html]
 
     return scores
 
 def get_fixtures():
     fixtures_container = get_container().find('h2', string="Fixtures").find_next_sibling()
     fixtures_html = fixtures_container.find_all('ul')
-    fixtures = [format_fixture(fixture) for fixture in fixtures_html]
+    fixtures = [format_html(fixture) for fixture in fixtures_html]
 
     return fixtures
 
-def format_score(score_html):
-    date = score_html.find_parent().find('h3').text
-    competition = score_html.find_parent().find('h4').text
+def get_today():
+    today_test = get_container().find('h2', string="Today")
+    if today_test:
+        today_html = today_test.find_next_sibling().find('ul')
+        today = format_html(today_html)
+
+        return today
+
+    return None
+
+def format_html(html):
+    dict = {}
+    date = html.find_parent().find('h3').text
     date = format_date(date)
-    home, away = score_html.find_all('span', class_='qa-full-team-name')
+    dict['date'] = date
 
-    score = {
-            "home_team":home.text,
-            "home_score":home.find_next().text,
-            "away_team":away.text,
-            "away_score":away.find_next().text,
-            "date":date,
-            "competition":competition
-            }
+    competition = html.find_parent().find('h4').text
+    dict['competition'] = competition
 
-    return score
+    home, away = html.find_all('span', class_='qa-full-team-name')
+    dict['home_team'] = home.text
+    dict['away_team'] = away.text
 
-def format_fixture(fixture_html):
-    date = fixture_html.find_parent().find('h3').text
-    date = format_date(date)
-    competition = fixture_html.find_parent().find('h4').text
-    time = fixture_html.find('span', class_='sp-c-fixture__number--time').text
-    home, away = fixture_html.find_all('span', class_='qa-full-team-name')
+    time_test = html.find('span', class_='sp-c-fixture__number--time')
+    if time_test:
+        dict['time'] = time_test.text
+    else:
+        dict['home_score'] = home.find_next().text
+        dict['away_score'] = away.find_next().text
 
-    fixture = {
-              "home_team":home.text,
-              "away_team":away.text,
-              "date":date,
-              "time":time,
-              "competition":competition
-              }
-
-    return fixture
+    return dict
 
 def format_date(unf_date):
     day, date, month, year = unf_date.split()
@@ -74,4 +72,3 @@ def format_date(unf_date):
 
 if __name__ == '__main__':
     print(get_scores())
-    print(get_fixtures())
